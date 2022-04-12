0f0cf2806d31b60094be9bac990e10d69036fa85
Riyad Hennaoui <riyad.hennaoui@gmail.com>
2022-04-12 08:58:04 +0200

    section 7 done Spring Configuration with Java Annotations - inversion of Control

---
+++ src/main/java/fr/afpa/springdemo/AnnontationDemoApp.java
@@ -0,0 +1,21 @@
+package fr.afpa.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnontationDemoApp {
+
+    public static void main(String[] args) {
+
+        // read spring config file
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // get the bean from spring container
+        Coach theCoach = context.getBean("tennisCoach", Coach.class);
+
+        //call a method on the bean
+        System.out.println(theCoach.getDailyWorkout());
+
+        //close the context
+        context.close();
+    }
+}
